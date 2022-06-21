46feda88c429a2b90f35b8959c141f651b5768e9
Marv <23shahm@elmbrookstudents.org>
2022-06-21 10:35:12 -0500

    Username and Age

---
+++ 01-starting-project/src/components/UI/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css";
+
+const Button = props => {
+    return (
+        <div>
+            <button type={props.type || "button"} onClick={props.ClickHandler}>{props.children}</button>
+        </div>
+    )
+};
+export default Button;
